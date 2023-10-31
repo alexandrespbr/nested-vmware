29a89a6e0bb7ed14bb131d5330367a31f5c73e74
Alexandre <andrdal@amazon.com>
2023-10-31 14:11:39 -0300

    Develop Branch Update

--- esxi-on-kvm/bash/configure_vcsas.sh
+++ esxi-on-kvm/bash/configure_vcsas.sh
@@ -42,10 +42,10 @@ VCSAJSONSOURCE=$ESXIROOT/JSON/vcsa-install-$VSPHEREVERSION.json
     sleep 30
 
     ## Deploy the vcsa appliances
-    mkdir -p /var/log/vcsa1 /var/log/vcsa2 
+    mkdir -p /var/log/vcsa1 /var/log/vcsa2
 
     $ESXIROOT/vcsa-extracted/$VSPHEREVERSION/vcsa-cli-installer/lin64/vcsa-deploy install $ESXIROOT/JSON/vcsa1.json --accept-eula --acknowledge-ceip --no-esx-ssl-verify --skip-ovftool-verification -v --log-dir=/var/log/vcsa1 > /dev/null 2>&1 &
-    $ESXIROOT/vcsa-extracted/$VSPHEREVERSION/vcsa-cli-installer/lin64/vcsa-deploy install $ESXIROOT/JSON/vcsa2.json --accept-eula --acknowledge-ceip --no-esx-ssl-verify --skip-ovftool-verification -v --log-dir=/var/log/vcsa2 > /dev/null 2>&1 &
+    #$ESXIROOT/vcsa-extracted/$VSPHEREVERSION/vcsa-cli-installer/lin64/vcsa-deploy install $ESXIROOT/JSON/vcsa2.json --accept-eula --acknowledge-ceip --no-esx-ssl-verify --skip-ovftool-verification -v --log-dir=/var/log/vcsa2 > /dev/null 2>&1 &
   
     # now wait until those are complete
     wait
